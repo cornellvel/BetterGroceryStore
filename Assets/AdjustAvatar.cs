99e8a217c90b13c377d820e312e1f6059cff7d53
Andrea Won <cornellvel@AG-COM-5DTVJB2.ag.cornell.edu>
2017-02-24 11:28:58 -0500

    first commit 2/24/2107

---
+++ Assets/AdjustAvatar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustAvatar : MonoBehaviour {
+
+    public GameObject CameraRig;
+    public GameObject Avatar;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        Avatar.transform.position = new Vector3((CameraRig.transform.position.x +3f), CameraRig.transform.position.y + 2f, CameraRig.transform.position.z);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/MimicHipsFemale.cs
@@ -0,0 +1,46 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class MimicHipsFemale : MonoBehaviour
+{
+
+    public GameObject followedObject;
+    public GameObject holdHeight;
+    public float theHeight;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.parent = followedObject.transform;
+        theHeight = holdHeight.transform.position.y;
+        Debug.Log(theHeight);
+    }
+
+    void followPosition()
+    {
+        Vector3 _tmp = followedObject.transform.position;
+        _tmp.x = (followedObject.transform.position.x - .14f);
+        _tmp.y = (followedObject.transform.position.y - (theHeight * .35f) + .0142f);
+        //_tmp.y = followedObject.transform.position.y - (theHeight * .35f);
+        //_tmp.y = 0;
+        _tmp.z = followedObject.transform.position.z;
+        this.transform.position = _tmp;
+    }
+    void followRotation()
+    {
+        Vector3 _tmp2 = followedObject.transform.eulerAngles;
+        _tmp2.x = 0;
+        //The body turns as a percentage of the head turning
+        _tmp2.y = followedObject.transform.eulerAngles.y;
+        //_tmp2.y = (followedObject.transform.eulerAngles.y * .75f);
+        _tmp2.z = 0;
+        this.transform.eulerAngles = _tmp2;
+    }
+    // Update is called once per frame
+
+    void Update()
+    {
+        followPosition();
+        followRotation();
+    }
+}
